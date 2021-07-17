83970ecfe9c5ca5b2eaa55db05d481ecb7ec3951
disco07 <koneenok@outlook.fr>
2021-07-17 20:24:25 +0200

    keybord finish

--- app/screens/SignIn/index.js
+++ app/screens/SignIn/index.js
@@ -1,85 +1,118 @@
-import React from 'react';
-import {Text, TouchableOpacity, View} from "react-native";
+import React, {useEffect, useState} from 'react';
+import {
+    Keyboard,
+    KeyboardAvoidingView,
+    Platform,
+    Text,
+    TouchableOpacity,
+    TouchableWithoutFeedback,
+    View
+} from "react-native";
 import styles from "./styles";
 import {LinearGradient} from 'expo-linear-gradient';
 import OutlineInput from 'react-native-outline-input';
 import {Icon} from 'react-native-elements'
 
 export default function SignIn({navigation}) {
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [keyboardStatus, setKeyboardStatus] = useState(false);
+
+    useEffect(() => {
+        Keyboard.addListener("keyboardDidShow", _keyboardDidShow);
+        Keyboard.addListener("keyboardDidHide", _keyboardDidHide);
+        // cleanup function
+        return () => {
+            Keyboard.removeListener("keyboardDidShow", _keyboardDidShow);
+            Keyboard.removeListener("keyboardDidHide", _keyboardDidHide);
+        };
+    }, []);
+    const _keyboardDidShow = () => Platform.OS === "android" && setKeyboardStatus(true);
+    const _keyboardDidHide = () => Platform.OS === "android" && setKeyboardStatus(false);
+
     return (
-        <View style={[styles.container]}>
-            <View style={{flex: 1, marginLeft: 20, justifyContent: "center"}}>
-                <Text style={{fontSize: 25, fontWeight: "bold"}}>Welcome,</Text>
-                <Text style={{fontSize: 25, opacity: 0.5}}>Sign in to continue!</Text>
-            </View>
-            <View style={styles.input}>
-                <View>
-                    <OutlineInput
-                        label="Username"
-                        activeValueColor="#6c63fe"
-                        activeBorderColor="#FF1493"
-                        activeLabelColor="#6c63fe"
-                        passiveBorderColor="#bbb7ff"
-                        passiveLabelColor="#bbb7ff"
-                        passiveValueColor="#bbb7ff"
-                    />
+        <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+            <View style={[styles.container]}>
+                <View style={{flex: 1, marginLeft: 20, justifyContent: "center"}}>
+                    <Text style={{fontSize: 25, fontWeight: "bold"}}>Welcome,</Text>
+                    <Text style={{fontSize: 25, opacity: 0.5}}>Sign in to continue!</Text>
                 </View>
-                <View style={{marginTop: 10}}>
-                    <OutlineInput
-                        label="Password"
-                        activeValueColor="#6c63fe"
-                        activeBorderColor="#FF1493"
-                        activeLabelColor="#6c63fe"
-                        passiveBorderColor="#bbb7ff"
-                        passiveLabelColor="#bbb7ff"
-                        passiveValueColor="#bbb7ff"
-                    />
+                <View style={styles.input}>
+                    <View>
+                        <OutlineInput
+                            value={email}
+                            onChangeText={(e) => setEmail(e)}
+                            label="Username"
+                            activeValueColor="#6c63fe"
+                            activeBorderColor="#FF1493"
+                            activeLabelColor="#6c63fe"
+                            passiveBorderColor="#bbb7ff"
+                            passiveLabelColor="#bbb7ff"
+                            passiveValueColor="#bbb7ff"
+                        />
+                    </View>
+                    <View style={{marginTop: 10}}>
+                        <OutlineInput
+                            value={password}
+                            onChangeText={(e) => setPassword(e)}
+                            label="Password"
+                            activeValueColor="#6c63fe"
+                            activeBorderColor="#FF1493"
+                            activeLabelColor="#6c63fe"
+                            passiveBorderColor="#bbb7ff"
+                            passiveLabelColor="#bbb7ff"
+                            passiveValueColor="#bbb7ff"
+                        />
+                    </View>
+                    <View style={{
+                        marginBottom: 5, fontSize: 10,
+                        width: "auto", height: "auto",
+                        fontWeight: "bold", alignItems: "flex-end"
+                    }}>
+                        <TouchableOpacity>
+                            <Text>
+                                Forget Password?
+                            </Text>
+                        </TouchableOpacity>
+                    </View>
+                    <View style={{marginTop: 40}}>
+                        <TouchableOpacity onPress={() => navigation.replace("Main")}>
+                            <LinearGradient
+                                start={{x: 0.0, y: 1.0}} end={{x: 1.0, y: 0.0}}
+                                colors={['#FF1493', '#FF69B4', '#FFC0CB']}
+                                style={styles.button}>
+                                <Text style={styles.text}>Login</Text>
+                            </LinearGradient>
+                        </TouchableOpacity>
+                        <TouchableOpacity>
+                            <View style={[styles.text, styles.button, {
+                                color: "blue",
+                                flexDirection: "row",
+                                justifyContent: 'center',
+                                backgroundColor: "#ADD8E6"
+                            }]}>
+                                <Text><Icon color={'blue'} name="facebook"/></Text>
+                                <Text style={{color: "blue"}}>Connect With Facebook</Text>
+                            </View>
+                        </TouchableOpacity>
+                    </View>
                 </View>
+                {
+                    keyboardStatus === false &&
                 <View style={{
-                    marginBottom: 5, fontSize: 10,
-                    width: "auto", height: "auto",
-                    fontWeight: "bold", alignItems: "flex-end"
+                    flex: 1,
+                    flexDirection: 'row',
+                    paddingBottom: 45,
+                    justifyContent: 'center',
+                    alignItems: 'flex-end'
                 }}>
-                    <TouchableOpacity>
-                        <Text>
-                            Forget Password?
-                        </Text>
+                    <Text>I'm a new user, </Text>
+                    <TouchableOpacity onPress={() => navigation.push("SignUp")}>
+                        <Text style={{color: "#FF1493"}}>Sign Up</Text>
                     </TouchableOpacity>
                 </View>
-                <View style={{marginTop: 40}}>
-                    <TouchableOpacity onPress={() => navigation.replace("Main")}>
-                        <LinearGradient
-                            start={{x: 0.0, y: 1.0}} end={{x: 1.0, y: 0.0}}
-                            colors={['#FF1493', '#FF69B4', '#FFC0CB']}
-                            style={styles.button}>
-                            <Text style={styles.text}>Login</Text>
-                        </LinearGradient>
-                    </TouchableOpacity>
-                    <TouchableOpacity>
-                        <View style={[styles.text, styles.button, {
-                            color: "blue",
-                            flexDirection: "row",
-                            justifyContent: 'center',
-                            backgroundColor: "#ADD8E6"
-                        }]}>
-                            <Text><Icon color={'blue'} name="facebook"/></Text>
-                            <Text style={{color: "blue"}}>Connect With Facebook</Text>
-                        </View>
-                    </TouchableOpacity>
-                </View>
+                }
             </View>
-            <View style={{
-                flex: 1,
-                flexDirection: 'row',
-                paddingBottom: 45,
-                justifyContent: 'center',
-                alignItems: 'flex-end'
-            }}>
-                <Text>I'm a new user, </Text>
-                <TouchableOpacity onPress={() => navigation.push("SignUp")}>
-                    <Text style={{color: "#FF1493"}}>Sign Up</Text>
-                </TouchableOpacity>
-            </View>
-        </View>
+        </TouchableWithoutFeedback>
     );
 };
