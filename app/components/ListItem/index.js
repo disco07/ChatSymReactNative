ef517b765a52fcbb5bf392224582c3c59bd779ca
disco07 <koneenok@outlook.fr>
2021-07-18 21:47:52 +0200

    ListItem begin

---
+++ app/components/ListItem/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {View} from "react-native";
+
+function ListItem() {
+    return (
+        <View>
+
+        </View>
+    );
+}
+
+export default ListItem;
--- app/components/UserAvatar/index.js
+++ app/components/UserAvatar/index.js
@@ -1,21 +1,22 @@
 import React from 'react';
-import {FlatList, Image, Text, View} from "react-native";
+import {FlatList, Image, ScrollView, Text, View} from "react-native";
 import styles from "./styles"
 
 function UserAvatar({data}) {
     return (
         <FlatList
             horizontal
+            showsHorizontalScrollIndicator={false}
             data={data}
             keyExtractor={item => item.id}
             renderItem={({item}) => (
                 <View>
-                <View style={styles.container}>
-                    <Image source={item.image} style={styles.image} />
-                </View>
-                <View style={styles.name}>
-                    <Text>{item.name}</Text>
-                </View>
+                    <View style={styles.container}>
+                        <Image source={item.image} style={styles.image}/>
+                    </View>
+                    <View style={styles.name}>
+                        <Text>{item.name}</Text>
+                    </View>
                 </View>
             )}/>
     );
